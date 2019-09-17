cb2f6dea5f7e1fb9499c1dd9d54c1f10ae70ed99
Carlos Banos <cbanos@travelbank.com>
2019-09-17 13:28:34 -0500

    Initial Commit

---
+++ AppleRSSTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleRSSTest
+//
+//  Created by Carlos Banos on 9/17/19.
+//  Copyright © 2019 Carlos Banos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
